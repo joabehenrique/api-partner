e4a1d3654e77216e859cf66299e058eee91fb41e
Joabe Henrique <joabe.souza@b2wdigital.com>
2022-08-20 12:25:37 -0300

    feat: create my entity migration

--- priv/repo/migrations/20220820150632_table_entity.exs
+++ priv/repo/migrations/20220820150632_table_entity.exs
@@ -2,6 +2,13 @@ defmodule ApiPartner.Repo.Migrations.TableEntity do
   use Ecto.Migration
 
   def change do
+    create table(:entity) do
+      add(:name, :string)
+      add(:entity_type, :string)
+      add(:inep, :integer)
+      add(:parent_id, :string)
 
+      timestamps()
+    end
   end
 end
