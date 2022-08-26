5c529fbd8e0916f9d459bb67f61031fbd5f0c606
Joabe Henrique <joabe.souza@b2wdigital.com>
2022-08-25 23:28:11 -0300

    feat: added handler insert when updating

--- lib/api_partner/schemas/entity/actions/update.ex
+++ lib/api_partner/schemas/entity/actions/update.ex
@@ -12,6 +12,11 @@ defmodule ApiPartner.Schemas.Entity.Actions.Update do
   defp update(entity, params) do
     entity
     |> Entity.changeset(params)
-    |> Repo.update!()
+    |> Repo.update()
+    |> handle_insert()
   end
+
+  defp handle_insert({:ok, %Entity{}} = result), do: result
+
+  defp handle_insert({:error, result}), do: {:error, Error.build(:bad_request, result)}
 end
