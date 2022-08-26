fcc88203e5b654a29049f42e9d61e0db876ba8ba
Joabe Henrique <joabe.souza@b2wdigital.com>
2022-08-25 23:26:54 -0300

    fix: fixed class rule without parent_id

--- lib/api_partner/schemas/entity/actions/create.ex
+++ lib/api_partner/schemas/entity/actions/create.ex
@@ -3,9 +3,10 @@ defmodule ApiPartner.Schemas.Entity.Actions.Create do
   alias ApiPartner.Schemas.EntitySchema, as: Entity
 
   def call(%{"parent_id" => parent_id} = params) do
-    case is_nil(parent_id) do
-      true -> validate_hierarchy(params)
-      _other_cases -> create(params)
+    if Blankable.blank?(parent_id) or is_nil(parent_id) do
+      validate_hierarchy(params)
+    else
+      create(params)
     end
   end
 
@@ -18,15 +19,11 @@ defmodule ApiPartner.Schemas.Entity.Actions.Create do
 
   defp validate_hierarchy(%{"entity_type" => "class"} = _params),
     do:
-      {:error,
-       Error.build(:bad_request, "Entity type is allowed null if it is network or school")}
+      {:error, Error.build(:bad_request, "Parent_id is allowed null if it is network or school")}
 
-  defp validate_hierarchy(%{"entity_type" => _entity_type} = params), do: create(params)
+  defp validate_hierarchy(params), do: create(params)
 
   defp handle_insert({:ok, %Entity{}} = result), do: result
 
-  defp handle_insert({:error, result}) do
-    IO.inspect(result, label: "\n\nAQUIIIs\n\n")
-    {:error, Error.build(:bad_request, result)}
-  end
+  defp handle_insert({:error, result}), do: {:error, Error.build(:bad_request, result)}
 end
